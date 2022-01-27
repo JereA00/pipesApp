43a97ab8a076ac5da920bdc636f7565a7ab65801
Jere Agustinoy <jagustinoy@encodesa.com.ar>
2022-01-27 13:46:39 -0300

    ruteo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { SharedModule } from './shared/shared.module';
+import { AppRouterModule } from './app-router.module';
+
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule,
+    AppRouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
